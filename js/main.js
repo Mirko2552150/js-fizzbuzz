12f8ee865ea9ce586e38bf96d0406842077d01d7
Mirko <mirko.longo.1987@gmail.com>
2020-02-20 14:23:44 +0100

    Inizio

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// SCOPO DEL GIOCO: Fizz Buzz test:
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+// --------------------------------------------------------------------------------------------------------------------------
+// Per essere più chiari l’obiettivo è questo:
+// 1
+// 2
+// Fizz
+// 4
+// Buzz
+// Fizz
+// 7
+// 8
+// Fizz
+// Buzz
+// 11
+// Fizz
+// 13
+// 14
+// FizzBuzz
+// ma non fino a 15, fino a 100!! Quindi quanti di voi avessero fatto l’esercizio di prima (Yes I’m looking to you!) scrivendo 5 righe con 5 richieste di prompt, ora sono messi alla prova muscolare :muscle:! Oppure decidete di impigrirvi come noi altri e di lasciar fare il lavoro pesante a queste fredde macchine su cui posiamo le mani.
+// Coraggio “the Spring is coming” se loro ci hanno fatto 8 stagioni con questo motto, noi possiamo concederci di usarlo ancora per qualche giorno :wink:
+// Buon lavoro a tutti e … attenti ai WhiteWalkers!
+// A domani
+//
+// Svolgimento in punti:
+// 1) Stampare N da una a 100
+// 2) Selezionare N multipli di 3 e stampare parola
+// 3) Selezionare N multipli di 5 e stampare Buzz
+// 4) Far si che i multipli in comune tra 3 e 5 stampino la parola Fizz-Buzz
